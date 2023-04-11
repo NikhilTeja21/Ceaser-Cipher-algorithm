abc7027ad5672a20bf3c1a309b62e025416880b5
NIKHIL TEJA <88529671+NikhilTeja21@users.noreply.github.com>
2023-04-11 08:10:07 +0530

    Updated Encryption_Decryption.java

--- Encryption_Decryption.java
+++ Encryption_Decryption.java
@@ -10,7 +10,6 @@ public class Encryption_Decryption    //File name
         {
             char pointer = Encrypt.charAt(chr);
             //If the character is upper case
-          
             if(Character.isUpperCase(pointer))
             {
                 int index = up.indexOf(pointer);
@@ -31,19 +30,18 @@ public class Encryption_Decryption    //File name
                 }
             }
         }
-      
         return Encrypt.toString();
     }
     
     
     public static void Decryption(String message,String low,String up,String lower,String upper)
     {
+        System.out.println("\n\nMessage before Decryption : "+Encrytion(message, low, up, lower, upper));
         StringBuilder Decrypt = new StringBuilder(message);
         for(int chr=0;chr<Decrypt.length();chr++)
         {
             char pointer = Decrypt.charAt(chr);
             //If the character is upper case
-          
             if(Character.isUpperCase(pointer))
             {
                 int index = upper.indexOf(pointer);
@@ -64,12 +62,10 @@ public class Encryption_Decryption    //File name
                 }
             }
         }
-      
         System.out.println("Message after decryption : "+Decrypt);
     }
     
-  
-    public static void Text_Conversion(String input,String lower,String upper,int private_key)
+    public static void Conversion(String input,String lower,String upper,int private_key)
     {
         Scanner ipt = new Scanner(System.in);
         String upper_converted_text = upper.substring(private_key)+upper.substring(0,private_key);   
@@ -82,31 +78,39 @@ public class Encryption_Decryption    //File name
         
         System.out.print("\n\nDo you want to decrypt the data ?? Press 1 : ");
         int press = ipt.nextInt();
-      
         if(press==1)
         {
             System.out.print("Enter the private key : ");
             int key = ipt.nextInt();
-          
+
+            if(key>26)
+            {
+                key = key%26;
+            }
+            if(key==26)
+            {
+                key = 21;
+            }
+
             if(key==private_key)
             Decryption(Encrytion(input,lower,upper,lower_converted_text,upper_converted_text),lower,upper,lower_converted_text,upper_converted_text);
             else
             {
                 System.out.println("Sorry, Wrong key entered. Please start from the beggining");
             }
-          
         }
-        else 
-        {
-            System.out.println("\n\n======Thank you======");
-        }
+
+
+        System.out.println("\n\n\t\t======Thank you======\n\n");
+        
     }
     
     
     public static void main(String... args)
     {
+        System.out.println("\n\n\t\tCeaser Cipher Algorithm\n");
         Scanner ipt = new Scanner(System.in);
-        System.out.print("\n\nEnter a private message : ");
+        System.out.print("\nEnter a private message : ");
         String input = ipt.nextLine();
       
         String lower_alpha = "abcdefghijklmnopqrstuvwxyz";    //If the character is lower converts into lower cipher text
@@ -114,10 +118,19 @@ public class Encryption_Decryption    //File name
       
         System.out.print("Enter a private key : ");
         int private_key = ipt.nextInt();
-      
+
+        if(private_key>26)
+        {
+            private_key = private_key%26;
+        }
+        if(private_key==26)
+        {
+            private_key = 21;
+        }
+        
         System.out.println("\n\nMessage before encryption : "+input);
         
-        Text_Conversion(input,lower_alpha,upper_alpha,private_key);
+        Conversion(input,lower_alpha,upper_alpha,private_key);
         
     }
 }
